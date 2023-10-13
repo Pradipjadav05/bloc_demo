b276b5ea090c6ea5a63f17b4eda6393613c5ccfd
Pradip Jadav <77873775+Pradipjadav05@users.noreply.github.com>
2023-10-13 14:57:10 +0530

    add block, state, event

---
+++ lib/bloc/internet_bloc.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'internet_bloc_event.dart';
+import 'internet_bloc_state.dart';
+
+class InternetBloc extends Bloc<InternetEvent, InternetState> {}
